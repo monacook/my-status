eaca64aaaa114a1429fe7a068b638ece0e58fead
monacook <cook.mona@gmail.com>
2021-05-03 18:05:45 -0700

    Added form and useState Hook

--- src/components/statusform.js
+++ src/components/statusform.js
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const StatusForm = () => {
+    const [message, setMessage] = useState("");
+    const [statusLists, setStatusLists] = useState([
+    {
+      status: "This is an example post.",
+      createdAt: new Date().toLocaleTimeString()
+    },
+    {
+      status: "Cool. Cool cool cool.",
+      createdAt: new Date().toLocaleTimeString()
+    },
+    {
+      status: "Good Luck!",
+      createdAt: new Date().toLocaleTimeString()
+    }
+]);
+
+const HandleFormSubmit = (e) => {
+    e.preventDefault();
+    setStatusLists([message]);
+    setMessage("");
+    console.log([statusLists]);
+  };
+
+
 return (
     <div className="wrapper">
-      <form>
+      <form onSubmit={HandleFormSubmit}>
         <textarea
           type="text"
-          value="What's on your mind?"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
         <button>Post</button>
       </form>
