feda9359cbc35a42c610f6101f32e0194fb070c3
monacook <cook.mona@gmail.com>
2021-05-06 17:33:29 -0700

    updated my language

--- src/Test/indexTest.js
+++ src/Test/indexTest.js
@@ -4,7 +4,7 @@ require("chromedriver");
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
     await driver.get("http://localhost:3001/");
-    await driver.findElement(By.name("q")).sendKeys("Hey DipShit. You dah best!");
+    await driver.findElement(By.name("q")).sendKeys("This is a test, but you dah best!");
     await driver.findElement(By.name("submit")).click();   
 };
 
