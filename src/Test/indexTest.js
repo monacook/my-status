373cad7300ec22a0a8060a6bd5df5142a68ed3f5
monacook <cook.mona@gmail.com>
2021-05-17 16:31:49 -0700

    added more interaction with HTML

--- src/Test/indexTest.js
+++ src/Test/indexTest.js
@@ -1,17 +1,20 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 require("chromedriver")
 
 const driver = new Builder()
     .forBrowser("chrome")
     .build();
 
-async function openWebsites() {
-    try{
-        await driver.get("http://yahoo.com");
-        await driver.get("http://google.com")
+async function example() {
+    try {
+        await driver.get("https://rori4.github.io/selenium-practice/#/pages/practice/simple-registration");
+        await driver.findElement(By.name("email")).sendKeys("mfkaj01@gmail.com");
+        await driver.findElement(By.name("password")).sendKeys("fdafdafdafda");
+        await driver.findElement(By.name("submit")).click();
+        
     } catch (error) {
-        console.log(error)
+     console.log(error)   
     }
 }
 
-openWebsites();
+example();
